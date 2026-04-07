72ed7a55abb5d9776ac87f451d0e9ac43e8d16f9
362Sadie-F <princessmae.fleming@gmail.com>
2026-04-07 13:01:42 -0400

    draw

--- Intro to MonoGame/Game1.cs
+++ Intro to MonoGame/Game1.cs
@@ -8,6 +8,10 @@ namespace Intro_to_MonoGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Rectangle window;
+        Texture2D dinoTexure;
+        Texture2D castleTexture;
+
 
         public Game1()
         {
@@ -20,12 +24,24 @@ namespace Intro_to_MonoGame
         {
             // TODO: Add your initialization logic here
 
+            window = new Rectangle(0, 0, 820, 620);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "Topic 1 Trial";
+
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            dinoTexure = Content.Load<Texture2D>("dino");
+
+            castleTexture = Content.Load<Texture2D>("castle");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -42,9 +58,17 @@ namespace Intro_to_MonoGame
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightSteelBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(dinoTexure, new Vector2(10, 10), Color.White);
+            _spriteBatch.Draw(dinoTexure, new Vector2(150, 100), Color.Thistle);
+            _spriteBatch.Draw(dinoTexure, new Vector2(280, 200), Color.Fuchsia);
+            _spriteBatch.Draw(castleTexture, new Vector2(350, -5), Color.Fuchsia);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
